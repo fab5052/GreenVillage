209c77f9671c594fa1ba69ffd99381629c86256b
fab5052 <f.beaujois@gmail.com>
2024-11-29 16:01:03 +0100

    cache

--- src/Entity/Trait/CreatedAtTrait.php
+++ src/Entity/Trait/CreatedAtTrait.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Entity\Trait\CreatedTrait;
+namespace App\Entity\Trait;
 
 use Doctrine\ORM\Mapping as ORM;
 
--- src/Entity/Trait/SlugTrait.php
+++ src/Entity/Trait/SlugTrait.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Entity\Trait\SlugTrait;
+namespace App\Entity\Trait;
 
 use Doctrine\ORM\Mapping as ORM;
 
